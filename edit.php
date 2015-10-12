287af73ce095b4a27d57e4b2a211b0299d5e5c6a
Karl Markus Wahlberg <skmw@greeny.cs.tlu.ee>
2015-10-12 13:29:23 +0300

    updating edit data through edit.php and edit_functions.php without table.php

--- edit.php
+++ edit.php
@@ -3,6 +3,11 @@
 	require_once("edit_functions.php"); //see on configi fail, mis viitab tabelile, ja edit kasutab seda.
 	//edit.php
 
+	
+	if(isset($_GET["update"])){
+		updateCarData($_GET["car_id"], $_GET["number_plate"], $_GET["color"]);
+		echo 
+	}
 	//trükin aadressirealt muutuja
 	
 	//siis ei anna errorit kui minna edit.php lehele ja pole lõpus kirjas edit_id
@@ -29,9 +34,9 @@
 
 <!--Salvestamiseks kasutan table.php rida updateCarData($_GET["car_id"], $_GET["number_plate"], $_GET["color"]); updateCar() -->
 
-<form action="table.php" method="get">
+<form action="edit.php" method="get">
 	<input name="car_id" type="hidden" value="<?=$_GET["edit_id"];?>">
-	<input name="number_plate" type="text" value="<?=$car->number_plate;?>"><br>
+	<input name="number_plate" type="text" value="<?=$car->number_plate;?>"><br> <!--siit läheb reale updateCarData. Siis läheb edit functions. $stmt->bind_param ja siis $stmt = $mysqli->prepare-->
 	<input name="color" type="text" value="<?=$car->color;?>"><br>
 	<input name="update" type="submit"><br>
 	
